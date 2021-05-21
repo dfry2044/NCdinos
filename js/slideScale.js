6894acd51e51c361ed582ea5886fd0c3bcd95d3a
이승열 <marudev4905@gmail.com>
2021-05-21 15:17:42 +0900

    slideScale.js 코드 수정

--- js/slideScale.js
+++ js/slideScale.js
@@ -3,13 +3,26 @@ var slideItem = $(".slide-item");
 
 var slideItemLength = slideItem.length;
 
+var i = -1;
+var num = 0;
 
-    var i = -1;
-    setInterval(function(){
-        if(i == 30){
-            i = -1;
-        }
-        slideItem.removeClass("scale");
-        i += 1;
-        slideItem.eq(i).addClass("scale");
-    }, 2000)
+setInterval(function(){
+    itemScale();
+}, 2000)
+
+setInterval(function(){
+    num += 20;
+
+    slideWrap.css({
+        left: 100 + num + 'vw' 
+    });
+}, 60000)
+
+function itemScale(){
+    if(i == 30){
+        i = -1;
+    }
+    slideItem.removeClass("scale");
+    i += 1;
+    slideItem.eq(i).addClass("scale");
+}
