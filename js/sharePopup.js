bdffacc89b2b840faa8ec52152fe81554161af08
이승열 <marudev4905@gmail.com>
2021-05-25 18:06:34 +0900

    picture-share.html 추가 및 퍼블리싱

---
+++ js/sharePopup.js
@@ -0,0 +1,13 @@
+var sharePopup = $(".share-popup");
+var sharePopupDim = $(".share-popup-dim");
+var shareOpenBtn = $(".share-link");
+var shareCloseBtn = $(".share-popup-close");
+
+shareOpenBtn.click(function(){
+    sharePopup.addClass("show");
+    sharePopupDim.addClass("show");
+});
+shareCloseBtn.click(function(){
+    sharePopup.removeClass("show");
+    sharePopupDim.removeClass("show");
+});
